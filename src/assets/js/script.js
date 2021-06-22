443a4ffd27357376d0212f7fcddb5e2f14b7187d
Sabancev Egor <60539979+egorsv1985@users.noreply.github.com>
2021-06-22 21:37:59 +0300

    added body class lock

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,8 +1,11 @@
 const headerBurgerBtn = document.querySelector('.header__burger-btn');
+const body = document.querySelector('body');
 if (headerBurgerBtn) {
 	const headerBurger = document.querySelector('.header__burger');
 	headerBurgerBtn.addEventListener("click", function () {
 		headerBurger.classList.toggle('_active');
+		body.classList.toggle('_lock');
 	});
 }
-console.log("hello");
+
+// найди элемент body через js запиши его в переменную и при нажатие на burger  body.classList.toggle('lock')  а в класс lock добавь overflow:hidden
